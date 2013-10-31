bc46299c10fa60e3f06a43f6be231fa0c5f3aa2a
Nick Sexton <nick.sexton@gmail.com>
2013-10-31 16:55:56 +0000

    now addressing data with index notation

--- c_reimplementation/pdp_structures.c
+++ c_reimplementation/pdp_structures.c
@@ -9,19 +9,22 @@
 
 /* A layer of units */
 typedef struct layer { 
-  double red;
-  double green;
-  double blue;
+
   struct layer * previous; // past iterations
   struct layer * next;     // future iterations
+  int size;
+  double * units; // pointer to array which contains our data
 } layer;
 
 
-layer * layer_create() {
+layer * layer_create(int size) {
   layer * new_layer;
   new_layer = (layer *)malloc (sizeof(layer));
   new_layer->previous = NULL;
   new_layer->next = NULL;
+  new_layer->size = size;
+  new_layer->units = (double *)malloc (size * (sizeof(double)));
+
   return new_layer;
 }
 
@@ -36,6 +39,7 @@ void layer_free_backward(layer * some_layer) {
     layer * tmp;
     tmp = some_layer->previous;
     printf ("freeing memory at %p\n", some_layer);
+    free (some_layer->units);
     free (some_layer);
     layer_free_backward(tmp); 
   }   
@@ -47,30 +51,29 @@ int main () {
   layer * a_layer_tail;
 
   /* a_layer = (layer *)malloc (sizeof(layer)); */ 
-  a_layer_tail = layer_create();
+  a_layer_tail = layer_create(3);
   a_layer_head = a_layer_tail;
-
-  a_layer_tail->red = 1;
-  a_layer_tail->green = 2;
-  a_layer_tail->blue = 3;
-
+  a_layer_tail->units[0] = 1;
+  a_layer_tail->units[1] = 2;
+  a_layer_tail->units[2] = 3;
 
   printf ("\n");
   printf ("red = %2.1f, green = %2.1f, blue = %2.1f\n", 
-	  a_layer_tail->red, a_layer_tail->green, a_layer_tail->blue);
+	  a_layer_tail->units[0], a_layer_tail->units[1], a_layer_tail->units[2]);
 
   /* now iterate the layer multiplying by 1.2 */
 
   int i;
   for (i = 0; i < 10; i++) {
     layer * tmp = a_layer_tail;
-    a_layer_tail = layer_create();
+    a_layer_tail = layer_create(3);
     a_layer_tail->previous = tmp;
     tmp->next = a_layer_tail;
 
-    a_layer_tail->red = a_layer_tail->previous->red * 1.2;
-    a_layer_tail->green = a_layer_tail->previous->green * 1.2;
-    a_layer_tail->blue = a_layer_tail->previous->blue * 1.2;
+    int u;
+    for (u = 0; u < a_layer_tail->size; u++) {
+      a_layer_tail->units[u] = a_layer_tail->previous->units[u] * 1.2;
+    }
   }
 
   
@@ -79,7 +82,7 @@ int main () {
   layer * layer_iterator = a_layer_tail;
   while (layer_iterator != NULL) {
     printf ("red = %2.1f, green = %2.1f, blue = %2.1f\n", 
-	    layer_iterator->red, layer_iterator->green, layer_iterator->blue);  
+	    layer_iterator->units[0], layer_iterator->units[1], layer_iterator->units[2]);  
     layer_iterator = layer_iterator->previous;
   }
 
@@ -88,7 +91,7 @@ int main () {
   layer_iterator = a_layer_head;
   while (layer_iterator != NULL) {
     printf ("red = %2.1f, green = %2.1f, blue = %2.1f\n", 
-	    layer_iterator->red, layer_iterator->green, layer_iterator->blue);  
+	    layer_iterator->units[0], layer_iterator->units[1], layer_iterator->units[2]);  
     layer_iterator = layer_iterator->next;
   }
 

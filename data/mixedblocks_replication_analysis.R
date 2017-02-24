17abfbd94e5bc8a714e5cc4865811f73f87d8f7c
Nick Sexton <nick.sexton@gmail.com>
2017-02-24 12:44:14 +0000

    updated mixed blocks analysis (4) to nicely link points representing each task

--- data/mixedblocks_replication_analysis.R
+++ data/mixedblocks_replication_analysis.R
@@ -3,14 +3,81 @@ rm (list=ls())
 
 library(ggplot2) # for graphs
 #library(pastecs) # for descriptive statistics
-#library(reshape2) # for transform
+library(reshape2) # for transform
 #library (plyr)
                                         # library(lsr) # for etaSquared
 
 imageDirectory <- file.path(Sys.getenv("HOME"), "Dropbox", "PhD", "Thesis", "replication")
 
 # 
-labels.data = c("response", "correct", "RT")
+labels.data = c("p1", "p2", "p3", "p4", "p5", "p6", "p7", "p8", "p9", "p10", "p11", "p12")
+
 
 fig4data.rt <- read.delim("fig4_rts.txt", header=FALSE, sep=c("", ":"), col.names=labels.data)
 fig4data.errors <- read.delim("fig4_errors.txt", header=FALSE, sep=c("", ":"), col.names=labels.data)
+
+fig4data.rt$block <- seq(from=1, by=1, to=nrow(fig4data.rt))
+fig4data.errors$block <- seq(from=1, by=1, to=nrow(fig4data.rt))
+
+
+
+fig4data.rt.long <- melt(fig4data.rt, id.vars = "block",
+                         variable.name = "trial.position",
+                         value.name = "RT")
+fig4data.errors.long <- melt(fig4data.errors,
+                             id.vars = "block",
+                             variable.name = "trial.position",
+                             value.name = "error.rate")
+
+idtask <- function (position) {
+    if (as.numeric(position) < 5) {
+        return (1)
+    } else if (as.numeric(position) < 9) {
+        return (2)
+    } else {
+        return (3)
+    }
+}
+
+                                        # convert p1, p2 etc. into numeric position
+    fig4data.rt.long$trial.position <- as.numeric(gsub('p', '', fig4data.rt.long$trial.position))
+
+# assign task (1, 2) for drawing group lines to match the figure
+fig4data.rt.long$task <- sapply (fig4data.rt.long$trial.position, idtask)
+    
+replication.fig4<- ggplot (fig4data.rt.long,  aes(x=trial.position, y=RT, group=task))
+replication.fig4 +
+    stat_summary(fun.y = mean, geom = "line", position = "dodge") +
+    stat_summary(fun.y = mean, geom = "point") +
+        stat_summary(fun.data = mean_cl_boot,
+                     geom = "errorbar",
+                     # position = position_dodge(width = 0.2),
+                     width = 0.2) + 
+    labs (x = "trial position", y = "Simulated RT (Cycles)") +
+    ggtitle("Replication: Gilbert & Shallice (2002) Fig. 4") +
+    theme_bw() + theme (legend.position="bottom") + 
+    scale_colour_grey(start = 0.1, end = 0.4) +
+    scale_y_continuous( name= "RT (model cycles)",
+       sec.axis = sec_axis(~ . * 5.8 +  318, # transformed sec axis to perform regression equation
+       name="simulated RT (ms)",
+       breaks = c(450, 550, 650, 750, 850, 950), 
+       labels = c("450", "550", "650", "750", "850", "950")))
+
+
+imageFile <- file.path("~/Dropbox/PhD/Thesis/replication/", "gs_fig4.png")
+ggsave(filename=imageFile, width = 200, height = 200, units = "mm")
+
+
+replication.fig4.errors<- ggplot (fig4data.errors.long,  aes(x=trial.position, y=error.rate))
+replication.fig4.errors +
+#    stat_summary(fun.y = mean, geom = "line", position = "dodge") +
+    stat_summary(fun.y = mean, geom = "point") +
+        stat_summary(fun.data = mean_cl_boot,
+                     geom = "errorbar",
+                     # position = position_dodge(width = 0.2),
+                     width = 0.2) + 
+      labs (x = "trial position", y = "Simulated RT (Cycles)") +
+          ggtitle("Replication: Gilbert & Shallice (2002) Fig. 4")
+
+imageFile <- file.path("~/Dropbox/PhD/Thesis/replication/", "gs_fig4.png")
+ggsave(filename=imageFile, width = 200, height = 200, units = "mm")
